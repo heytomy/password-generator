93b5c01c55c267b0029ef197a782a2fc7519321c
Thomas NISI <nisi.thomas@outlook.fr>
2023-09-08 12:13:30 +0200

    update

--- index.php
+++ index.php
@@ -1,96 +1,97 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Générateur de mot de passe</title>
 </head>
 
 <body>
     <h1>Générateur de mots de passe</h1>
-    <!-- Formulaire permettant à l'utilisateur de choisir les paramètres du mot de passe -->
+    <!-- Form allowing the user to choose password parameters -->
     <form action="" method="post">
         <label for="length">Longueur du mot de passe :</label>
-        <!-- Slider permettant de choisir la longueur du mot de passe (de 8 à 32 caractères) -->
-        <input type="range" name="length" id="length" min="12" max="32" value="12">
+        <input type="range" name="length" id="length" min="8" max="32" value="12">
+        <span id="lengthValue">12</span> caractères
         <br>
         <label for="special_chars">Inclure des caractères spéciaux :</label>
-        <!-- Checkbox permettant de choisir si le mot de passe doit inclure des caractères spéciaux -->
-        <input type="checkbox" name="special_chars" id="special_chars" value="1">
+        <input type="checkbox" name="special_chars" id="special_chars" value="1" checked>
         <br>
         <label for="letters">Inclure des lettres :</label>
-        <!-- Checkbox permettant de choisir si le mot de passe doit inclure des lettres -->
-        <input type="checkbox" name="letters" id="letters" value="1">
+        <input type="checkbox" name="letters" id="letters" value="1" checked>
         <br>
         <label for="numbers">Inclure des chiffres :</label>
-        <!-- Checkbox permettant de choisir si le mot de passe doit inclure des chiffres -->
-        <input type="checkbox" name="numbers" id="numbers" value="1">
+        <input type="checkbox" name="numbers" id="numbers" value="1" checked>
         <br>
         <label for="symbols">Inclure des symboles :</label>
-        <!-- Checkbox permettant de choisir si le mot de passe doit inclure des symboles -->
-        <input type="checkbox" name="symbols" id="symbols" value="1">
+        <input type="checkbox" name="symbols" id="symbols" value="1" checked>
         <br>
-        <!-- Bouton de soumission du formulaire -->
         <input type="submit" value="Générer le mot de passe">
     </form>
 
     <?php
-    // Fonction pour générer un mot de passe sécurisé
-    function generatePassword($length, $special_chars, $letters, $numbers, $symbols)
-    {
-        $chars = '';
-
-        // Si l'utilisateur veut des caractères spéciaux, on ajoute ces caractères à la liste de caractères possibles
-        if ($special_chars) {
-            $chars .= '!@#$%^&*';
-        }
-
-        // Si l'utilisateur veut des lettres, on ajoute les lettres minuscules et majuscules à la liste de caractères possibles
-        if ($letters) {
-            $chars .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        }
-
-        // Si l'utilisateur veut des chiffres, on ajoute les chiffres à la liste de caractères possibles
-        if ($numbers) {
-            $chars .= '0123456789';
-        }
-
-        // Si l'utilisateur veut des symboles, on ajoute ces symboles à la liste de caractères possibles
-        if ($symbols) {
-            $chars .= '!@#$%^&*()';
-        }
-
-        // On initialise le mot de passe à vide
-        $password = '';
-
-        // On génère aléatoirement un caractère à chaque itération de la boucle
-        for ($i = 0; $i < $length; $i++) {
-            // On choisit aléatoirement un caractère dans la liste de caractères possibles
-            $password .= $chars[mt_rand(0, strlen($chars) - 1)];
-        }
-
-        // On retourne le mot de passe généré
-        return $password;
+// Function to generate a secure password
+function generatePassword($length, $special_chars, $letters, $numbers, $symbols)
+{
+    // Check if at least one character set is selected
+    if (!$special_chars && !$letters && !$numbers && !$symbols) {
+        return "Veuillez sélectionner au moins un jeu de caractères.";
     }
 
-    // Si le formulaire a été soumis
-    if (isset($_POST['length'])) {
-        // On récupère les valeurs des paramètres choisis par l'utilisateur
-        $length = (int)$_POST['length'];
-        $special_chars = (bool)$_POST['special_chars'];
-        $letters = (bool)$_POST['letters'];
-        $numbers = (bool)$_POST['numbers'];
-        $symbols = (bool)$_POST['symbols'];
-
-        // On génère le mot de passe en utilisant la fonction PHP generatePassword
-        $password = generatePassword($length, $special_chars, $letters, $numbers, $symbols);
+    // Define character sets
+    $charset = '';
+    if ($special_chars) {
+        $charset .= '!@#$%^&*';
     }
-        // On affiche le mot de passe généré à l'utilisateur
-        echo "<p>Mot de passe généré : $password</p>";
+    if ($letters) {
+        $charset .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if ($numbers) {
+        $charset .= '0123456789';
+    }
+    if ($symbols) {
+        $charset .= '!@#$%^&*()';
+    }
 
-    ?>
+    // Generate the password securely
+    $password = '';
+    $charset_length = strlen($charset);
+    for ($i = 0; $i < $length; $i++) {
+        $random_index = random_int(0, $charset_length - 1);
+        $password .= $charset[$random_index];
+    }
 
+    return $password;
+}
+
+// If the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Validate and sanitize user input
+    $length = isset($_POST['length']) ? max(8, min(32, (int)$_POST['length'])) : 12;
+    $special_chars = isset($_POST['special_chars']) ? true : false;
+    $letters = isset($_POST['letters']) ? true : false;
+    $numbers = isset($_POST['numbers']) ? true : false;
+    $symbols = isset($_POST['symbols']) ? true : false;
+
+    // Generate the password using the PHP generatePassword function
+    $password = generatePassword($length, $special_chars, $letters, $numbers, $symbols);
+
+    // Display the generated password to the user
+    echo "<p>Mot de passe généré : $password</p>";
+}
+?>
+
+
+    <script>
+        // Display the current value of the length slider
+        const lengthSlider = document.getElementById("length");
+        const lengthValue = document.getElementById("lengthValue");
+        lengthSlider.addEventListener("input", () => {
+            lengthValue.textContent = lengthSlider.value;
+        });
+    </script>
 </body>
 </html>
